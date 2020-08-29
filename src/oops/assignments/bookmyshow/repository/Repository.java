656c74b10514ef9408c4bdeebc3fa2a1467f082c
amrojsandhu <amrojpals@gmail.com>
2020-08-29 15:25:00 +0530

    Using concurrent hash map

--- src/oops/assignments/bookmyshow/repository/Repository.java
+++ src/oops/assignments/bookmyshow/repository/Repository.java
@@ -1,13 +1,14 @@
 package oops.assignments.bookmyshow.repository;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Repository<T, K> {
 
     Map<K, T> map;
 
     protected Repository() {
-        this.map = new HashMap<>();
+        this.map = new ConcurrentHashMap<>();
     }
 
     public void save(T obj, K id) {
